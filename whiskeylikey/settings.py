9d362150763c66adf91af108fe197ac2a38e158f
JBH <xz7840@dgu.edu>
2023-11-11 11:24:23 +0900

    배포 세팅

--- whiskeylikey/settings.py
+++ whiskeylikey/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9#o1d+gewe8m)5@@9q=n=ms@2_66r@en*_(1cdxvd+%6)&^nk9"
+with open('secret.json', 'r') as f:
+    secret = json.loads(f.read())
 
+def get_secret(setting, secret=secret):
+    try:
+        return secret[setting]
+    except:
+        msg = "Set key '{0}' in secret.json".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+# USAGE
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret('SECRET_KEY') # my-secret-key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -76,11 +87,12 @@ CORS_ALLOWED_ORIGINS = [
 	'http://localhost:8000',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    # 프론트 주소 추가
 ]
 
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_ALL_ORIGINS: True
 
 ROOT_URLCONF = "whiskeylikey.urls"
 
@@ -149,11 +161,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
